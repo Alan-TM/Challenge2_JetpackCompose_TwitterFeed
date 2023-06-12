47f5f0cdfe2b0d9215ffe1a4c8955847b2e776ec
Alan Torres Morales <alantorresm86@gmail.com>
2023-06-12 12:19:15 -0700

    devs solution

--- app/src/main/java/com/alan/challenge2_jetpackcompose_twitterfeed/MainActivity.kt
+++ app/src/main/java/com/alan/challenge2_jetpackcompose_twitterfeed/MainActivity.kt
@@ -3,13 +3,37 @@ package com.alan.challenge2_jetpackcompose_twitterfeed
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.alan.challenge2_jetpackcompose_twitterfeed.ui.theme.Challenge2_JetpackCompose_TwitterFeedTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +42,150 @@ class MainActivity : ComponentActivity() {
         setContent {
             Challenge2_JetpackCompose_TwitterFeedTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Column(
+                    modifier = Modifier.fillMaxSize().background(Color(0xFF161026))
                 ) {
-                    Greeting("Android")
+                    TwitterCard()
+                    TuitDivider()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TwitterCard() {
+    var chat by remember {
+        mutableStateOf(false)
+    }
+
+    var rt by remember {
+        mutableStateOf(false)
+    }
+
+    var like by remember {
+        mutableStateOf(false)
+    }
+
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF161026))
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "",
+            modifier = Modifier
+                .clip(
+                    CircleShape
+                )
+                .size(55.dp)
+        )
+
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Row(Modifier.fillMaxWidth()) {
+                TextTile(title = "Aris", Modifier.padding(8.dp))
+                DefaultText(title = "@AristiDevs", Modifier.padding(8.dp))
+                DefaultText(title = "4h", Modifier.padding(8.dp))
+                Spacer(modifier = Modifier.weight(1f))
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_dots),
+                    contentDescription = "",
+                    tint = Color.White
+                )
+            }
+
+            TextBody(
+                text = "Esto es una frase larga jaja jiji" +
+                    "Esto es una frase larga jaja jiji" +
+                    "Esto es una frase larga jaja jiji" +
+                    "Esto es una frase larga jaja jiji" +
+                    "Esto es una frase larga jaja jiji" +
+                    "Esto es una frase larga jaja jiji" +
+                    "Esto es una frase larga jaja jiji",
+                Modifier.padding(bottom = 8.dp)
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .clip(
+                        RoundedCornerShape(10)
+                    ),
+                contentScale = ContentScale.Crop
+            )
+
+            Row(Modifier.fillMaxWidth().padding(top = 16.dp)) {
+                SocialIcon(
+                    modifier = Modifier.weight(1f),
+                    unselectedIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_chat),
+                            contentDescription = "",
+                            tint = Color(0xFF7E8B98)
+                        )
+                    },
+                    selectedIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_chat_filled),
+                            contentDescription = "",
+                            tint = Color(0xFF7E8B98)
+                        )
+                    },
+                    isSelected = chat
+                ) {
+                    chat = !chat
+                }
+
+                SocialIcon(
+                    modifier = Modifier.weight(1f),
+                    unselectedIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_rt),
+                            contentDescription = "",
+                            tint = Color(0xFF7E8B98)
+                        )
+                    },
+                    selectedIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_rt),
+                            contentDescription = "",
+                            tint = Color(0xFF00FF27)
+                        )
+                    },
+                    isSelected = rt
+                ) {
+                    rt = !rt
+                }
+
+                SocialIcon(
+                    modifier = Modifier.weight(1f),
+                    unselectedIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_like),
+                            contentDescription = "",
+                            tint = Color(0xFF7E8B98)
+                        )
+                    },
+                    selectedIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_like_filled),
+                            contentDescription = "",
+                            tint = Color(0xFFFF0000)
+                        )
+                    },
+                    isSelected = like
+                ) {
+                    like = !like
                 }
             }
         }
@@ -30,14 +193,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SocialIcon(
+    modifier: Modifier,
+    unselectedIcon: @Composable () -> Unit,
+    selectedIcon: @Composable () -> Unit,
+    isSelected: Boolean,
+    onItemSelected: () -> Unit
+) {
+    val defaultValue = 1
+
+    Row(modifier.clickable { onItemSelected() }, verticalAlignment = Alignment.CenterVertically) {
+        if (isSelected) {
+            selectedIcon()
+        } else {
+            unselectedIcon()
+        }
+
+        Text(
+            text = if (isSelected) (defaultValue + 1).toString() else defaultValue.toString(),
+            color = Color(0xFF7E8B98),
+            fontSize = 12.sp,
+            modifier = Modifier.padding(start = 4.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    Challenge2_JetpackCompose_TwitterFeedTheme {
-        Greeting("Android")
-    }
+fun TextBody(text: String, modifier: Modifier = Modifier) {
+    Text(text = text, color = Color.White, modifier = modifier)
+}
+
+@Composable
+fun TextTile(title: String, modifier: Modifier = Modifier) {
+    Text(text = title, color = Color.White, fontWeight = FontWeight.ExtraBold, modifier = modifier)
+}
+
+@Composable
+fun DefaultText(title: String, modifier: Modifier = Modifier) {
+    Text(text = title, color = Color.Gray, modifier = modifier)
+}
+
+@Composable
+fun TuitDivider() {
+    Divider(
+        Modifier.padding(top = 4.dp).height(1.dp).fillMaxWidth(),
+        color = Color(0xFF7E8B98)
+    )
 }
